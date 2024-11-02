73d01f5836e96e6fc66787123279218d599919f9
DavidT <davidt.info@gmail.com>
2024-11-02 09:15:31 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatch_frases/Frase.java
@@ -0,0 +1,66 @@
+package br.com.alura.screenmatch_frases;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "frases")
+public class Frase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String frase;
+    private String personagem;
+    private String titulo;
+    private String poster;
+
+    public Frase() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public String getPersonagem() {
+        return personagem;
+    }
+
+    public void setPersonagem(String personagem) {
+        this.personagem = personagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", frase='" + frase + '\'' +
+                ", personagem='" + personagem + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", poster='" + poster + '\'';
+    }
+}
